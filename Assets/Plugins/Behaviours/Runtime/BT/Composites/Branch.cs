dc11a00c9edbc41a6a8dbf79cf296d77c70a82e8
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2024-07-19 10:55:06 +0200

    Implement branch composite

--- Assets/Plugins/Behaviours/Runtime/BT/Composites/Branch.cs
+++ Assets/Plugins/Behaviours/Runtime/BT/Composites/Branch.cs
@@ -1,16 +1,35 @@
 ﻿using Jackey.Behaviours.Attributes;
 using Jackey.Behaviours.Core;
+using Jackey.Behaviours.Core.Conditions;
+using UnityEngine;
 
 namespace Jackey.Behaviours.BT.Composites {
 	[GraphIcon("Branch")]
 	[SearchPath("Composites/Branch")]
 	public class Branch : Composite {
+		[SerializeField] private BehaviourConditionGroup m_conditions;
+
+		private int m_runningIndex = -1;
+
 #if UNITY_EDITOR
 		protected internal override int Editor_MaxChildCount => 2;
 #endif
 
+		protected override ExecutionStatus OnEnter() {
+			m_conditions.Enable(Owner);
+			m_runningIndex = m_conditions.Evaluate() ? 0 : 1;
+			m_conditions.Disable();
+
+			return ExecutionStatus.Running;
+		}
+
 		protected override ExecutionStatus OnTick() {
-			return ExecutionStatus.Success;
+			BehaviourAction runningChild = m_children[m_runningIndex];
+			if (runningChild.IsFinished)
+				return (ExecutionStatus)runningChild.Status;
+
+			Debug.Assert(runningChild.Status == ActionStatus.Inactive);
+			return runningChild.EnterSequence();
 		}
 	}
 }
