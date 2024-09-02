ac619508d6d471d610944d7569bd5fe3db3969fa
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2024-09-02 15:41:51 +0200

    Add ability to tick custom components

---
+++ Assets/Plugins/Behaviours/Runtime/BT/Actions/IComponentAction.cs
@@ -0,0 +1,19 @@
+﻿namespace Jackey.Behaviours.BT.Actions {
+	public interface IComponentAction {
+		ExecutionStatus OnEnter();
+		ExecutionStatus OnTick();
+
+		void OnInterrupt();
+		void OnResult(ActionResult result);
+		void OnExit();
+	}
+
+	public interface IComponentAction<T> {
+		ExecutionStatus OnEnter(T args);
+		ExecutionStatus OnTick(T args);
+
+		void OnInterrupt(T args);
+		void OnResult(T args, ActionResult result);
+		void OnExit(T args);
+	}
+}
---
+++ Assets/Plugins/Behaviours/Runtime/BT/Actions/TickComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Jackey.Behaviours.Core;
+using UnityEngine;
+
+namespace Jackey.Behaviours.BT.Actions {
+	[SearchPath("Utilities/Tick Component")]
+	public class TickComponent : BehaviourAction<MonoBehaviour> {
+		protected override ExecutionStatus OnEnter() => GetInterfaceTarget().OnEnter();
+		protected override ExecutionStatus OnTick() => GetInterfaceTarget().OnTick();
+
+		protected override void OnInterrupt() => GetInterfaceTarget().OnInterrupt();
+		protected override void OnResult(ActionResult result) => GetInterfaceTarget().OnResult(result);
+		protected override void OnExit() => GetInterfaceTarget().OnExit();
+
+		private IComponentAction GetInterfaceTarget() {
+			MonoBehaviour component = GetTarget();
+
+			if (component is not IComponentAction target)
+				throw new InvalidOperationException($"Component \"{component}\" must implement {nameof(IComponentAction)} to be able to be ticked from a behaviour");
+
+			return target;
+		}
+	}
+}
