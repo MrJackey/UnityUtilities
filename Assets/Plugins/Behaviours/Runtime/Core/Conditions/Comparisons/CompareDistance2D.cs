facc2142acbb951e2a8a971edbb1db5b3a7c1d34
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2024-09-15 15:15:46 +0200

    Add distance compare conditions

---
+++ Assets/Plugins/Behaviours/Runtime/Core/Conditions/Comparisons/CompareDistance2D.cs
@@ -0,0 +1,21 @@
+﻿using Jackey.Behaviours.Attributes;
+using Jackey.Behaviours.Core.Blackboard;
+using Jackey.Behaviours.Utilities;
+using UnityEngine;
+
+namespace Jackey.Behaviours.Core.Conditions.Comparisons {
+	[SearchPath("Utilities/Compare Vector2 Distance")]
+	public class CompareDistance2D : BehaviourCondition {
+		public BlackboardRef<Vector2> From;
+		public BlackboardRef<Vector2> To;
+		public Arithmetic.Comparison Comparison;
+		public BlackboardRef<float> Comparand;
+
+		public override string Editor_Info => $"{From.Editor_Info} => {To.Editor_Info} {Arithmetic.GetComparisonString(Comparison)} {Comparand.Editor_Info}";
+
+		public override bool Evaluate() {
+			float comparand = Comparand.GetValue();
+			return Arithmetic.Compare(Vector2.SqrMagnitude(To.GetValue() - From.GetValue()), Comparison, comparand * comparand);
+		}
+	}
+}
---
+++ Assets/Plugins/Behaviours/Runtime/Core/Conditions/Comparisons/CompareDistance3D.cs
@@ -0,0 +1,21 @@
+﻿using Jackey.Behaviours.Attributes;
+using Jackey.Behaviours.Core.Blackboard;
+using Jackey.Behaviours.Utilities;
+using UnityEngine;
+
+namespace Jackey.Behaviours.Core.Conditions.Comparisons {
+	[SearchPath("Utilities/Compare Vector3 Distance")]
+	public class CompareDistance3D : BehaviourCondition {
+		public BlackboardRef<Vector3> From;
+		public BlackboardRef<Vector3> To;
+		public Arithmetic.Comparison Comparison;
+		public BlackboardRef<float> Comparand;
+
+		public override string Editor_Info => $"{From.Editor_Info} => {To.Editor_Info} {Arithmetic.GetComparisonString(Comparison)} {Comparand.Editor_Info}";
+
+		public override bool Evaluate() {
+			float comparand = Comparand.GetValue();
+			return Arithmetic.Compare(Vector3.SqrMagnitude(To.GetValue() - From.GetValue()), Comparison, comparand * comparand);
+		}
+	}
+}
