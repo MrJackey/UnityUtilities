7c88b16470aa9ae6aca3ba91aaf8271ba046ef6f
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2024-09-26 22:59:07 +0200

    Replace deserialize null check with type check
    This removes unity API usage when deserializing off the main thread

--- Assets/Plugins/Behaviours/Runtime/Core/Blackboard/BlackboardVar.cs
+++ Assets/Plugins/Behaviours/Runtime/Core/Blackboard/BlackboardVar.cs
@@ -97,7 +97,7 @@ namespace Jackey.Behaviours.Core.Blackboard {
 			m_value = (BlackboardValue)Activator.CreateInstance(valueType);
 
 			if (typeof(Object).IsAssignableFrom(serializedType))
-				m_value.SetValueBoxed(m_unityObjectValue != null ? m_unityObjectValue : null); // Ensure Unity's fake null isn't set as value. If it is, the cast when setting boxed value fails
+				m_value.SetValueBoxed(m_unityObjectValue.GetType() != typeof(Object) ? m_unityObjectValue : null); // Ensure Unity's fake null isn't set as value. If it is, the cast when setting boxed value fails. Unity null / missing are of type Object
 			else if (!string.IsNullOrEmpty(m_primitiveValue) && (serializedType == typeof(string) || serializedType.IsPrimitive))
 				m_value.SetValueBoxed(Convert.ChangeType(m_primitiveValue, serializedType));
 			else if (m_boxedValue != null && serializedType.IsInstanceOfType(m_boxedValue))
