74a0efe221251651026cc58801e4311223d0323d
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-02-07 14:59:33 +0100

    Add Vector2 extension adding z component

--- Assets/Scripts/Utilities/Extensions/Vector3Extensions.cs
+++ Assets/Scripts/Utilities/Extensions/Vector3Extensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace Jackey.Utilities.Extensions {
 	public static class Vector3Extensions {
@@ -17,31 +18,37 @@ namespace Jackey.Utilities.Extensions {
 		/// <summary>
 		/// Returns a Vector3 copy giving x a new value
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithX(this Vector3 vec, float x) => new Vector3(x, vec.y, vec.z);
 
 		/// <summary>
 		/// Returns a Vector3 copy giving y a new value
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithY(this Vector3 vec, float y) => new Vector3(vec.x, y, vec.z);
 
 		/// <summary>
 		/// Returns a Vector3 copy giving z a new value
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithZ(this Vector3 vec, float z) => new Vector3(vec.x, vec.y, z);
 
 		/// <summary>
 		/// Returns a Vector3 copy giving x and y new values
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithXY(this Vector3 vec, float x, float y) => new Vector3(x, y, vec.z);
 
 		/// <summary>
 		/// Returns a Vector3 copy giving x and z new values
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithXZ(this Vector3 vec, float x, float z) => new Vector3(x, vec.y, z);
 
 		/// <summary>
 		/// Returns a Vector3 copy giving y and z new values
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static Vector3 WithYZ(this Vector3 vec, float y, float z) => new Vector3(vec.x, y, z);
 	}
 }
