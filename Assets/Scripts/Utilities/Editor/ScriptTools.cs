c1d6f71b05307daa15fee39f1ad65eb1016515e0
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-02-16 14:44:19 +0100

    Add menu item to reload script assemblies

---
+++ Assets/Scripts/Utilities/Editor/ScriptTools.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Jackey.Utilities.Editor {
+	public static class ScriptTools
+	{
+		[MenuItem("Tools/Jackey/Scripts/Reload Script Assemblies", false, 0)]
+		private static void ReloadScriptAssemblies()
+		{
+			EditorUtility.RequestScriptReload();
+		}
+	}
+}
