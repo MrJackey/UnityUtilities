a0422916d7af4fff64b05762b126e41054b44df4
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-02-16 14:43:59 +0100

    Add utility to load asset from GUID

---
+++ Assets/Scripts/Utilities/Editor/AssetUtilities.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Jackey.Utilities.Editor {
+	public static class AssetUtilities {
+		/// <summary>
+		/// Shortcut for converting a GUID to an asset path and immediately loading the asset
+		/// </summary>
+		/// <param name="guid">The GUID of the asset</param>
+		/// <returns>The asset with the input GUID</returns>
+		public static T LoadAssetWithGUID<T>(string guid) where T : Object {
+			return AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
+		}
+	}
+}
