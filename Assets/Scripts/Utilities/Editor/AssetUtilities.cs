608b8f89c2bd43fa46826258cff1102f8125f4ad
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-02-24 16:23:27 +0100

    Add utility to get a script's asset

--- Assets/Scripts/Utilities/Editor/AssetUtilities.cs
+++ Assets/Scripts/Utilities/Editor/AssetUtilities.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using System;
+using JetBrains.Annotations;
+using UnityEditor;
+using Object = UnityEngine.Object;
 
 namespace Jackey.Utilities.Editor {
 	public static class AssetUtilities {
@@ -10,6 +12,26 @@ namespace Jackey.Utilities.Editor {
 		/// <returns>The asset with the input GUID</returns>
 		public static T LoadAssetWithGUID<T>(string guid) where T : Object {
 			return AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid));
+		}
+
+		/// <inheritdoc cref="GetScriptAsset"/>
+		[CanBeNull]
+		public static MonoScript GetScriptAsset<T>() => GetScriptAsset(typeof(T));
+
+		/// <summary>
+		/// Get the asset associated to a specific script type. If the script has multiple types,
+		/// the asset is only returned if it's the first one.
+		/// </summary>
+		/// <param name="type">The script to search for</param>
+		/// <returns>The asset associated to the given script type. Returns null if none is found</returns>
+		[CanBeNull]
+		public static MonoScript GetScriptAsset(Type type) {
+			foreach (MonoScript script in MonoImporter.GetAllRuntimeMonoScripts()) {
+				if (script.GetClass() == type)
+					return script;
+			}
+
+			return null;
 		}
 	}
 }
