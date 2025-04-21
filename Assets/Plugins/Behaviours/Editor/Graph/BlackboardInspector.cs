b0fdee90c64c9c083dddef4b028a434fa229ef53
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-04-21 10:59:10 +0200

    Add labels distinguishing owner and graph blackboards

--- Assets/Plugins/Behaviours/Editor/Graph/BlackboardInspector.cs
+++ Assets/Plugins/Behaviours/Editor/Graph/BlackboardInspector.cs
@@ -6,20 +6,23 @@ using UnityEngine.UIElements;
 
 namespace Jackey.Behaviours.Editor.Graph {
 	public class BlackboardInspector : VisualElement {
+		private Label m_primaryLabel;
 		private PropertyField m_primaryBlackboardField;
+		private Label m_secondaryLabel;
 		private PropertyField m_secondaryBlackboardField;
-
-		public override VisualElement contentContainer { get; }
 
 		public BlackboardInspector() {
 			style.position = Position.Absolute;
 			style.maxHeight = Length.Percent(100f);
 
-			hierarchy.Add(new Label("Blackboard") { name = "Header" });
-			hierarchy.Add(m_primaryBlackboardField = new PropertyField());
-			hierarchy.Add(m_secondaryBlackboardField = new PropertyField() {
-				name = "SecondaryBlackboard",
-			});
+			Add(new Label("Blackboard") { name = "Header" });
+			Add(m_primaryLabel = new Label("Owner") { style = { display = DisplayStyle.None }});
+			m_primaryLabel.AddToClassList("BlackboardHeader");
+			Add(m_primaryBlackboardField = new PropertyField() { name = "PrimaryBlackboard" });
+
+			Add(m_secondaryLabel = new Label("Graph") { style = { display = DisplayStyle.None }});
+			m_secondaryLabel.AddToClassList("BlackboardHeader");
+			Add(m_secondaryBlackboardField = new PropertyField());
 
 			m_primaryBlackboardField.RegisterCallback<MouseDownEvent>(evt => evt.StopImmediatePropagation());
 			m_secondaryBlackboardField.RegisterCallback<MouseDownEvent>(evt => evt.StopImmediatePropagation());
@@ -28,21 +31,30 @@ namespace Jackey.Behaviours.Editor.Graph {
 		}
 
 		public void SetPrimaryBlackboard(Blackboard blackboard, SerializedProperty property) {
+			m_primaryLabel.style.display = DisplayStyle.Flex;
+			m_primaryBlackboardField.style.display = DisplayStyle.Flex;
 			m_primaryBlackboardField.BindProperty(property);
 			Blackboard.Available[0] = blackboard;
+
+			m_secondaryLabel.style.display = DisplayStyle.Flex;
 		}
 
 		public void SetSecondaryBlackboard(Blackboard blackboard, SerializedProperty property) {
+			m_secondaryBlackboardField.style.display = DisplayStyle.Flex;
 			m_secondaryBlackboardField.BindProperty(property);
 			Blackboard.Available[1] = blackboard;
 		}
 
 		public void ClearBlackboards() {
+			m_primaryLabel.style.display = DisplayStyle.None;
 			m_primaryBlackboardField.Unbind();
 			m_primaryBlackboardField.Clear();
+			m_primaryBlackboardField.style.display = DisplayStyle.None;
 
+			m_secondaryLabel.style.display = DisplayStyle.None;
 			m_secondaryBlackboardField.Unbind();
 			m_secondaryBlackboardField.Clear();
+			m_secondaryBlackboardField.style.display = DisplayStyle.None;
 
 			for (int i = 0; i < Blackboard.Available.Length; i++)
 				Blackboard.Available[i] = null;
