05975b4c69ee4025f526aa21cf5ca53bc7cb3fbb
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-05-24 12:01:40 +0200

    Create Do Until composite

---
+++ Assets/Plugins/Behaviours/Runtime/BT/Composites/DoUntil.cs
@@ -0,0 +1,59 @@
+﻿using Jackey.Behaviours.Attributes;
+
+namespace Jackey.Behaviours.BT.Composites {
+	[DisplayName("Do Until")]
+	[SearchPath("Composites/Do Until")]
+	public class DoUntil : Composite {
+		private int m_runningIndex;
+
+		protected override ExecutionStatus OnEnter() {
+			// Check overriding children
+			for (int i = 1; i < m_children.Count; i++) {
+				ExecutionStatus overrideStatus = m_children[i].EnterSequence();
+				if (overrideStatus == ExecutionStatus.Success)
+					return ExecutionStatus.Success;
+
+				if (overrideStatus == ExecutionStatus.Running) {
+					m_runningIndex = i;
+					return ExecutionStatus.Running;
+				}
+			}
+
+			// Start the base child
+			ExecutionStatus childStatus = m_children[0].EnterSequence();
+			if (childStatus == ExecutionStatus.Running) {
+				m_runningIndex = 0;
+				EnableTicking();
+			}
+
+			return childStatus;
+		}
+
+		protected override ExecutionStatus OnTick() {
+			for (int i = 1; i < m_children.Count; i++) {
+				ExecutionStatus overrideStatus = m_children[i].EnterSequence();
+				if (overrideStatus == ExecutionStatus.Success)
+					return ExecutionStatus.Success;
+
+				if (overrideStatus == ExecutionStatus.Running) {
+					m_children[0].Interrupt();
+
+					DisableTicking();
+					m_runningIndex = i;
+					return ExecutionStatus.Running;
+				}
+			}
+
+			return ExecutionStatus.Running;
+		}
+
+		protected override ExecutionStatus OnChildFinished() {
+			return (ExecutionStatus)m_children[m_runningIndex].Status;
+		}
+
+		protected override void OnExit() {
+			if (m_runningIndex == 0)
+				DisableTicking();
+		}
+	}
+}
