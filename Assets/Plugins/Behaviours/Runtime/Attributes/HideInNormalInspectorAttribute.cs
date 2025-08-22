9ce9c024f5079922feb9b444eac5292a43afc448
Jacob Lundberg <44698252+MrJackey@users.noreply.github.com>
2025-08-22 16:11:11 +0200

    Add HideInNormalInspectorAttribute

---
+++ Assets/Plugins/Behaviours/Runtime/Attributes/HideInNormalInspectorAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jackey.Behaviours.Attributes {
+	/// <summary>
+	/// Hides a field in the inspector but keeps it visible in the debug view
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)]
+	[Conditional("UNITY_EDITOR")]
+	public sealed class HideInNormalInspectorAttribute : PropertyAttribute { }
+
+	#if UNITY_EDITOR
+	namespace PropertyDrawers {
+		[CustomPropertyDrawer(typeof(HideInNormalInspectorAttribute))]
+		public class HideInNormalInspectorPropertyDrawer : PropertyDrawer {
+			public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) { }
+
+			public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => 0f;
+		}
+	}
+	#endif
+}
